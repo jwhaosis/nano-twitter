bebfe4310ef3f5b6be43a5aa137c4ad68d433067
jwhaosis <jwhaosis@gmail.com>
2018-03-19 19:14:21 -0400

    merge

--- app.rb
+++ app.rb
@@ -1,5 +1,4 @@
 require 'sinatra'
-require 'sinatra/activerecord'
 require './app/controllers/app_controller'
 require './app/controllers/tweets_controller'
 require './app/controllers/users_controller'
