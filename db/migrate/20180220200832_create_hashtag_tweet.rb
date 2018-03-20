8b908860fe29b4bf6688d02ffac20413e3a6fbe4
jwhaosis <jwhaosis@gmail.com>
2018-03-19 20:28:14 -0400

    get rid of hashtagtweet

--- db/migrate/20180220200832_create_hashtag_tweet.rb
+++
@@ -1,8 +0,0 @@
-class CreateHashtagTweets < ActiveRecord::Migration[5.1]
-  def change
-    create_table :hashtag_tweets do |t|
-      t.integer :hashtag_id
-      t.integer :tweet_id
-    end
-  end
-end
