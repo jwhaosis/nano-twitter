e29ced7c9af916de508c60a7fcaf082cecadbf77
Shu Lin Chan <shulin.chan@gmail.com>
2018-03-19 20:37:52 -0400

    bcyrpt needs require_relative in class

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   has_many :followers, source: :followed_by_id
   has_many :following, class_name: 'Follower', foreign_key: :user_id
   has_many :likes
-  has_many :mentions
+  has_many :mentionsh
   has_many :hashtags
 
   def User.digest string
