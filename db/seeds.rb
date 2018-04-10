7942388fedc0384b39bfff76bf681baca4b5172b
jwhaosis <jwhaosis@gmail.com>
2018-04-10 18:54:58 -0400

    query and import

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'activerecord-import'
 require_relative '../app/models/user'
 require_relative '../app/models/follower'
 require_relative '../app/models/tweet'
@@ -16,34 +17,49 @@ Like.delete_all
 Tweettag.delete_all
 Mention.delete_all
 
+users = []
+followers = []
+tweets = []
+count = 0
+
 #load users
 CSV.foreach('./db/seeds/users.csv') do |user|
   username = user[1].downcase.gsub(/\W+/, '')
-  User.create(
+  users << User.new(
+      id: count+=1,
       name: username,
       email: "#{username}@email.com",
       password: "password"
   )
 end
 
+count = 0
 #load followers
 CSV.foreach('./db/seeds/follows.csv') do |follow|
   user_id = follow[0]
   follows_user_id = follow[1]
-  Follower.create(
+  followers << Follower.new(
+      id: count+=1,
       user_id: follows_user_id.to_i,
       followed_by_id: user_id.to_i
   )
 end
 
+count = 0
 #load tweets
 CSV.foreach('./db/seeds/tweets.csv') do |tweets_row|
   user_id = tweets_row[0]
   tweet = tweets_row[1]
   created_at = tweets_row[2]
-  Tweet.create(
+  tweets << Tweet.new(
+      id: count+=1,
       user_id: user_id.to_i,
       tweet: tweet,
       created_at: Date.parse(created_at)
   )
 end
+
+User.import users
+Follower.import followers
+Tweet.import tweets
+Tweet.order!(:created_at  )
