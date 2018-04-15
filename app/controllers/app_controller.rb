c3d870e5c38b1f0ae00cb6abcdffb55c58f02c33
jwhaosis <jwhaosis@gmail.com>
2018-04-15 15:59:24 -0400

    queries for homepage

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -3,7 +3,10 @@ enable :sessions
 helpers SessionsHelper
 
 get '/' do
-  @tweets = logged_in? ? current_user.following_tweets(session[:user_id]).order(:created_at).first(50) : Tweet.joins(:user).select("tweets.*, users.name").first(50)
+  @logged_in_tweets = Tweet.where(user_id: Follower.where(followed_by_id: 1).to_a.map{|value| value.user_id}) if logged_in?
+  @tweets = Tweet.joins(:user).select("tweets.*, users.name").first(50)
+  @likes_hash = Like.group(:tweet_id).count
+  @retweets_hash = Tweet.group(:retweet_id).count
   erb :"app_pages/home"
 end
 
