54294d12ae50e819b673e1aecfb7183a1250fd08
Shu Lin Chan <shulin.chan@gmail.com>
2018-04-22 10:54:18 -0400

    Feed and timeline toggle on homepage

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -13,6 +13,14 @@ get '/' do
   end
 end
 
+get '/feed' do
+  byebug
+  # @tweets = generic_tweet_cache
+  @tweets = Tweet.where(user_id: Follower.where(followed_by_id: session[:user_id]).to_a.map{|value| value.user_id})
+  @user_likes = Like.where(user_id: session[:user_id]).select(:tweet_id).to_a.map{|value| value.tweet_id}
+  erb :"app_pages/home"
+end
+
 post '/search' do
   search params[:search]
   erb :"app_pages/search"
