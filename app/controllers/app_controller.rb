8b44e8dd35fc7b35084a9a98761e7c325c6f9f34
jwhaosis <jwhaosis@gmail.com>
2018-04-24 14:30:44 -0400

    get

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -40,7 +40,7 @@ get '/user/testuser' do
   erb :'user_pages/user_tweets'
 end
 
-post '/user/testuser/tweet' do
+get '/user/testuser/tweet' do
   EM.run {
     request = EM::HttpRequest.new("#{ENV['DB_HELPER']}/create/tweet/1001").post
     EM.stop
